ed5511adbb4e45dc92d7cbad259c0a9c5f94de64
Comic Solvency <131397277+ComicBurduli@users.noreply.github.com>
2025-01-22 19:12:21 +0400

    Add files via upload

---
+++ first_lesson L1 for Youtube/first_lesson L1 for Youtube/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_lesson_L1_for_Youtube
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+           int myNum = 15;
+
+            double myNum2 = 9.657328832434334344343434433434; 
+            float myNum3 = 9.503233234244242F;    
+
+            
+            Console.WriteLine(myNum2);
+
+
+
+
+
+        }
+    }
+}
